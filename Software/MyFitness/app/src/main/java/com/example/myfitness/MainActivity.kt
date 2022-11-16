bf47e8b76ae9a472d1ba7582952b3466af2cc6c0
timjuic <70685646+timjuic@users.noreply.github.com>
2022-11-16 19:56:02 +0100

    implementirana glavna navigacija i fragmenti

--- Software/MyFitness/app/src/main/java/com/example/myfitness/MainActivity.kt
+++ Software/MyFitness/app/src/main/java/com/example/myfitness/MainActivity.kt
@@ -2,10 +2,66 @@ package com.example.myfitness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.myfitness.adapters.MainPagerAdapter
+import com.example.myfitness.fragments.CalendarFragment
+import com.example.myfitness.fragments.ExercisesFragment
+import com.example.myfitness.fragments.ProfileFragment
+import com.example.myfitness.fragments.ProgressFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager2: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tabs)
+        viewPager2 = findViewById(R.id.viewpager)
+
+        val mainPagerAdapter = MainPagerAdapter(supportFragmentManager, lifecycle)
+
+        mainPagerAdapter.addFragment(
+            MainPagerAdapter.FragmentItem(
+                R.string.exercises_fragment_title,
+                R.drawable.ic_baseline_fitness_center_24,
+                ExercisesFragment::class
+            )
+        )
+
+        mainPagerAdapter.addFragment(
+            MainPagerAdapter.FragmentItem(
+                R.string.calendar_fragment_title,
+                R.drawable.ic_baseline_calendar_month_24,
+                CalendarFragment::class
+            )
+        )
+
+        mainPagerAdapter.addFragment(
+            MainPagerAdapter.FragmentItem(
+                R.string.progress_fragment_title,
+                R.drawable.ic_baseline_show_chart_24,
+                ProgressFragment::class
+            )
+        )
+
+        mainPagerAdapter.addFragment(
+            MainPagerAdapter.FragmentItem(
+                R.string.profile_fragment_title,
+                R.drawable.ic_baseline_person_24,
+                ProfileFragment::class
+            )
+        )
+
+        viewPager2.adapter = mainPagerAdapter
+
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            tab.setText(mainPagerAdapter.fragmentItems[position].titleRes)
+            tab.setIcon(mainPagerAdapter.fragmentItems[position].iconRes)
+        }.attach()
     }
 }
