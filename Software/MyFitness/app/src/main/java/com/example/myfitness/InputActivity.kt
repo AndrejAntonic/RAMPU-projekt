62ff82f02b8c80e4744ed2e6c10d5b7a958ba393
jgasi20 <jgasi20@foi.hr>
2022-11-17 23:18:03 +0100

    Kreirana aktivnost unosa podataka te njena xml datoteka. U xml datoteci kreiran je kostur unosa podataka te privremeni dizajn.

---
+++ Software/MyFitness/app/src/main/java/com/example/myfitness/InputActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myfitness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfitness.R
+
+class InputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
