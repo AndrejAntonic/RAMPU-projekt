1d55fc4728d045f4f4ff7fe3fef83ce3025c0fb7
jgasi20 <jgasi20@foi.hr>
2023-01-18 22:44:28 +0100

    Privremeni commit openButton activity-a.

--- Software/MyFitness/app/src/main/java/com/example/myfitness/fragments/CalculatorFragment.kt
+++ Software/MyFitness/app/src/main/java/com/example/myfitness/fragments/CalculatorFragment.kt
@@ -1,14 +1,20 @@
 package com.example.myfitness.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.myfitness.InputActivity
+import com.example.myfitness.LoginActivity
 import com.example.myfitness.R
 
 
 class CalculatorFragment : Fragment() {
+
+    lateinit var openButton : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +22,14 @@ class CalculatorFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calculator, container, false)
+
+
+        openButton.setOnClickListener{
+            requireActivity().run{
+                startActivity(Intent(this, InputActivity::class.java))
+                finish()
+            }
+        }
     }
 
 
