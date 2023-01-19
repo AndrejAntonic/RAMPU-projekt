78785a41e97bf29802d66964ab637a9346cb6eff
tjuic20-foi <tim.juic@student.foi.hr>
2023-01-19 16:55:36 +0100

    Dodan model "DoneExercise" DoneExercise je blueprint za dodavanje napravljenih vjezbi na treningu

---
+++ Software/MyFitness/app/src/main/java/model/DoneExercise.kt
@@ -0,0 +1,11 @@
+package model
+
+import java.util.*
+
+data class DoneExercise(
+    val exerciseName : String,
+    val weight : Int,
+    val sets : Int,
+    val reps : Int,
+    val date : Date
+)
