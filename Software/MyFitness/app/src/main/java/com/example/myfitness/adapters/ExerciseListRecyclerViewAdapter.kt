581bd9795a092ce77a6860badb502e23cfdc47cb
tjuic20-foi <tim.juic@student.foi.hr>
2023-01-20 22:29:48 +0100

    Lista za odabir napravljene vjezbe prebacena iz ListView u RecycleView

---
+++ Software/MyFitness/app/src/main/java/com/example/myfitness/adapters/ExerciseListRecyclerViewAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.myfitness.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myfitness.R
+
+class ExerciseListRecyclerViewAdapter(private val mList: List<String>) : RecyclerView.Adapter<ExerciseListRecyclerViewAdapter.ViewHolder>() {
+    private var listener: OnItemClickListener? = null
+    private var filteredItems: List<String> = mList.toList()
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_done_exercise_dialog, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val item = filteredItems[position]
+        holder.textView.text = item
+//        holder.imageView.setImageResource(ItemsViewModel.image)
+
+        holder.itemView.setOnClickListener {
+            listener?.onItemClick(position, item)
+        }
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return filteredItems.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.card_imageview)
+        val textView: TextView = itemView.findViewById(R.id.card_done_exercise_dialog_textview)
+    }
+
+    fun filter(query: String) {
+        filteredItems = mList.filter {
+            it.contains(query, ignoreCase = true)
+        }
+        notifyDataSetChanged()
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int, item: String)
+    }
+    fun setOnItemClickListener(listener: OnItemClickListener?) {
+        this.listener = listener
+    }
+}
--- Software/MyFitness/app/src/main/java/com/example/myfitness/helpers/AddDoneExerciseDialogHelper.kt
+++ Software/MyFitness/app/src/main/java/com/example/myfitness/helpers/AddDoneExerciseDialogHelper.kt
@@ -4,25 +4,24 @@ import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.Context
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ListView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myfitness.DataAccessObjects.DoneExercisesDAO
 import com.example.myfitness.DataAccessObjects.ExerciseDAO
 import com.example.myfitness.R
+import com.example.myfitness.adapters.ExerciseListRecyclerViewAdapter
 import model.DoneExercise
-import model.Exercise
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AddDoneExerciseDialogHelper(private val dialog: AlertDialog, private val context: Context) {
 
     val searchExerciseEditText : EditText = dialog.findViewById(R.id.exercisePicker)
-    val searchListView : ListView = dialog.findViewById(R.id.listview_exercises)
+    val recycleView : RecyclerView = dialog.findViewById(R.id.rv_exercise_picker)
     var selectedExercise : String = ""
     val saveButton : Button = dialog.findViewById(R.id.btn_save_doneexercise_dialog)
     val cancelButton : Button = dialog.findViewById(R.id.btn_cancel_doneexercise_dialog)
@@ -38,32 +37,55 @@ class AddDoneExerciseDialogHelper(private val dialog: AlertDialog, private val c
 
     suspend fun load() {
 
-        var arrayAdapter : ArrayAdapter<*> = ArrayAdapter(context, R.layout.listview_item, allExerciseNames)
+//        var arrayAdapter : ArrayAdapter<*> = ArrayAdapter(context, R.layout.card_done_exercise_dialog, allExerciseNames)
 
         allExerciseNames = ExerciseDAO.getAllExerciseNames()
 
-        arrayAdapter = ArrayAdapter(context, R.layout.listview_item, allExerciseNames)
-        searchListView.adapter = arrayAdapter
+        recycleView.layoutManager = LinearLayoutManager(context)
+
+        val adapter = ExerciseListRecyclerViewAdapter(allExerciseNames)
+        recycleView.adapter = adapter
+
+//        arrayAdapter = ArrayAdapter(context, R.layout.card_done_exercise_dialog, allExerciseNames)
+//        searchListView.adapter = arrayAdapter
 
 
-        searchExerciseEditText.addTextChangedListener {
-            println()
-            if (it.toString().isNotEmpty()) {
-                searchListView.visibility = View.VISIBLE
-                arrayAdapter.filter.filter(it.toString())
+        adapter.setOnItemClickListener(object : ExerciseListRecyclerViewAdapter.OnItemClickListener {
+            override fun onItemClick(position: Int, item: String) {
+                // handle click event
+                println(item)
+                println(position)
 
+                selectedExercise = item
+                searchExerciseEditText.setText(selectedExercise)
+                recycleView.visibility = View.GONE
+            }
+        })
+
+        searchExerciseEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                recycleView.visibility = View.VISIBLE
             } else {
-                searchListView.visibility = View.GONE
+                recycleView.visibility = View.GONE
             }
         }
 
-        searchListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-            // This is your listview's selected item
-            selectedExercise = parent.getItemAtPosition(position) as String
-            println(selectedExercise)
-            searchExerciseEditText.setText(selectedExercise)
-            searchListView.visibility = View.GONE
+        searchExerciseEditText.addTextChangedListener {
+            if (it.toString().isNotEmpty()) {
+                recycleView.visibility = View.VISIBLE
+                adapter.filter(it.toString())
+            } else {
+                recycleView.visibility = View.GONE
+            }
         }
+
+//        recycleView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+//            // This is your listview's selected item
+//            selectedExercise = parent.getItemAtPosition(position) as String
+//            println(selectedExercise)
+//            searchExerciseEditText.setText(selectedExercise)
+//            recycleView.visibility = View.GONE
+//        }
 
 
         saveButton.setOnClickListener {
