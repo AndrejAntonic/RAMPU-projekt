7f6d18eed2ec0deb1c37a681fae08494b2af7337
mkljaic20 <mkljaic20@foi.hr>
2023-01-20 17:58:53 +0100

    Napravljen dizajn za uređivanje profila i dodana logika potrebna za to.

--- Software/MyFitness/app/src/main/java/com/example/myfitness/DataAccessObjects/UsersDAO.kt
+++ Software/MyFitness/app/src/main/java/com/example/myfitness/DataAccessObjects/UsersDAO.kt
@@ -1,6 +1,7 @@
 package com.example.myfitness.DataAccessObjects
 
 
+import android.util.Log
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.ktx.firestore
@@ -20,9 +21,9 @@ object UsersDAO {
          return db.collection("users").add(user)
     }
 
-    fun Edituser(username: String, email: String, password: String, Height: Double, Weight: Double, Age: Int, Activity: Int, gender: String, metabolicRate: Double) {
-
-    }
+//    fun Edituser(username: String, email: String, password: String, Height: Double, Weight: Double, Age: Int, Activity: Int, gender: String, metabolicRate: Double) {
+//
+//    }
 
     suspend fun GetUserByUsername(username: String) : Boolean {
         val db = Firebase.firestore
@@ -50,5 +51,21 @@ object UsersDAO {
             .get().await()
 
         return user.size() > 0
+    }
+
+    suspend fun EditUser(username: String, email: String, password: String, name: String, lastName: String) {
+        val db = Firebase.firestore
+        val userRef = db.collection("users").whereEqualTo("username", username)
+        val document = userRef.get().await()
+        if (document.size() > 0) {
+            val user = document.documents[0].reference
+            user.update("username", username).await()
+            user.update("email", email).await()
+            user.update("password", password).await()
+            user.update("name", name).await()
+            user.update("lastName", lastName).await()
+        } else {
+            Log.d("EditUser", "Nije moguce promijeniti podatke")
+        }
     }
 }
--- Software/MyFitness/app/src/main/java/com/example/myfitness/fragments/EditProfileFragment.kt
+++ Software/MyFitness/app/src/main/java/com/example/myfitness/fragments/EditProfileFragment.kt
@@ -1,6 +1,52 @@
 package com.example.myfitness.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.myfitness.DataAccessObjects.UsersDAO
+import com.example.myfitness.R
+import kotlinx.coroutines.launch
 
-class EditProfileFragment: Fragment() {
+class EditProfileFragment : Fragment() {
+
+    private lateinit var etName: EditText
+    private lateinit var etLastName: EditText
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var btnEditUser: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_edit_profile, container, false)
+
+        etName = v.findViewById(R.id.userNameEdit)
+        etLastName = v.findViewById(R.id.userLastNameEdit)
+        etUsername = v.findViewById(R.id.usernameEdit)
+        etPassword = v.findViewById(R.id.passwordEdit)
+        etEmail = v.findViewById(R.id.emailEdit)
+        btnEditUser = v.findViewById(R.id.saveChangesBtn)
+
+        btnEditUser.setOnClickListener {
+            val name = etName.text.toString()
+            val lastName = etLastName.text.toString()
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+            val email = etEmail.text.toString()
+
+            viewLifecycleOwner.lifecycleScope.launch {
+                UsersDAO.EditUser(username, email, password, name, lastName)
+            }
+        }
+
+        return v
+    }
 }
+
