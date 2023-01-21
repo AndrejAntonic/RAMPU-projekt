2cb7db346d4797b8f928ed59c7dae459056db2c8
tjuic20-foi <tim.juic@student.foi.hr>
2023-01-21 16:31:46 +0100

    Implementirana metoda za dohvacanje napravljenih vjezbi za trenutnog (prijavljenog) korisnika

--- Software/MyFitness/app/src/main/java/com/example/myfitness/DataAccessObjects/DoneExercisesDAO.kt
+++ Software/MyFitness/app/src/main/java/com/example/myfitness/DataAccessObjects/DoneExercisesDAO.kt
@@ -3,7 +3,9 @@ package com.example.myfitness.DataAccessObjects
 
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 import model.DoneExercise
+import model.Exercise
 
 object DoneExercisesDAO {
     fun add(doneExercise: DoneExercise) : Boolean {
@@ -20,6 +22,26 @@ object DoneExercisesDAO {
             return true
         } catch (e: Exception) {
             return false
+        }
+    }
+
+    suspend fun getAllDoneExercisesForUser(username : String) : MutableList<DoneExercise> {
+        val db = Firebase.firestore
+        val exercisesList = mutableListOf<DoneExercise>()
+
+        val exercises = db.collection("doneExercises").document(username).collection("savedExercises")
+
+        try {
+            val result = exercises.get().await()
+            for (document in result) {
+                val exercise = document.toObject(DoneExercise::class.java)
+                println("Exercise:")
+                println(exercise)
+                exercisesList.add(exercise)
+            }
+            return exercisesList
+        } catch (e: Exception) {
+            return mutableListOf()
         }
     }
 }
