3b0213a6174d309d201484e553ed360089e42a95
jgasi20 <jgasi20@foi.hr>
2023-01-21 21:29:44 +0100

    Popravljen layout za unos podataka, inicijalizirani podaci u InputActivity.

--- Software/MyFitness/app/src/main/java/com/example/myfitness/InputActivity.kt
+++ Software/MyFitness/app/src/main/java/com/example/myfitness/InputActivity.kt
@@ -10,51 +10,23 @@ import java.math.RoundingMode
 
 class InputActivity : AppCompatActivity() {
 
-    lateinit var display : TextView
     lateinit var editWeight : EditText
     lateinit var editHeight : EditText
     lateinit var editAge : EditText
-    lateinit var genderMale : Button
-    lateinit var genderFemale : Button
+    lateinit var editActivity : EditText
+    lateinit var editGender : EditText
+    lateinit var btnSubmit : Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input)
 
-        display = findViewById(R.id.display_result)
         editWeight = findViewById(R.id.edit_weight)
         editHeight = findViewById(R.id.edit_height)
         editAge = findViewById(R.id.edit_age)
-        genderMale = findViewById(R.id.male)
-        genderFemale = findViewById(R.id.female)
-
-        genderMale.setOnClickListener{
-            val firstEdit = editWeight.text.toString().toInt()
-            val secondEdit = editHeight.text.toString().toInt()
-            val thirdEdit = editAge.text.toString().toInt()
-            Male(firstEdit, secondEdit, thirdEdit)
-        }
-
-        genderFemale.setOnClickListener {
-            val firstEdit = editWeight.text.toString().toInt()
-            val secondEdit = editHeight.text.toString().toInt()
-            val thirdEdit = editAge.text.toString().toInt()
-            Female(firstEdit, secondEdit, thirdEdit)
-        }
-    }
-
-    private fun Male(firstEdit: Int, secondEdit: Int, thirdEdit: Int)
-    {
-        val result = 88.362 + (13.397 * firstEdit) + (4.799 * secondEdit) - (5.677 * thirdEdit)
-        val decimal = BigDecimal(result).setScale(3, RoundingMode.HALF_EVEN)
-        display.text = decimal.toString()
-    }
-
-    private fun Female(firstEdit: Int, secondEdit: Int, thirdEdit: Int)
-    {
-        val result = 447.593 + (9.247 * firstEdit) + (3.098 * secondEdit) - (4.330 * thirdEdit)
-        val decimal = BigDecimal(result).setScale(3, RoundingMode.HALF_EVEN)
-        display.text = decimal.toString()
+        editActivity = findViewById(R.id.edit_activity)
+        editGender = findViewById(R.id.edit_gender)
+        btnSubmit = findViewById(R.id.btn_Submit)
     }
 }
